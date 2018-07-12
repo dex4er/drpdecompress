1a523589ea76681e618012c33bebf72744f5f7c0
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2018-07-12 21:56:19 +0200

    `-h` or `--help`; input and output file as arguments; calloc; status

--- drpdecompress.c
+++ drpdecompress.c
@@ -17,18 +17,52 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <ucl/ucl.h>
 
 #define MAX_BUFFER 10240
 #define SKIP_BYTES 4
 
-int main () {
-    unsigned char input[MAX_BUFFER], output[MAX_BUFFER];
-
+int main (int argc, char *argv[])  {
+    unsigned char *input, *output;
+    FILE *fin, *fout;
     ucl_uint sizein, sizeout;
+    int status;
 
-    if (!(sizein = fread(input, 1, sizeof input, stdin))) {
+    if (argc > 1) {
+        if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+            printf("Usage: %s [input [output]]\n", argv[0]);
+            exit(0);
+        }
+        if (!(fin = fopen(argv[1], "rb"))) {
+            fprintf(stderr, "Cannot open input file\n");
+            exit(1);
+        }
+    } else {
+        fin = stdin;
+    }
+
+    if (argc > 2) {
+        if (!(fout = fopen(argv[2], "wb"))) {
+            fprintf(stderr, "Cannot open output file\n");
+            exit(1);
+        }
+    } else {
+        fout = stdout;
+    }
+
+    if (!(input = calloc(MAX_BUFFER, 1))) {
+        fprintf(stderr, "Cannot allocate memory for input\n");
+        exit(1);
+    }
+
+    if (!(output = calloc(MAX_BUFFER, 1))) {
+        fprintf(stderr, "Cannot allocate memory for output\n");
+        exit(1);
+    }
+
+    if (!(sizein = fread(input, 1, MAX_BUFFER-1, fin))) {
         fprintf(stderr, "Cannot read from input\n");
         exit(1);
     }
@@ -36,13 +70,20 @@ int main () {
         fprintf(stderr, "Input is not valid\n");
         exit(1);
     }
+    fclose(fin);
 
-    ucl_nrv2e_decompress_8(input + SKIP_BYTES, sizein - SKIP_BYTES, output, &sizeout, NULL);
+    if ((status = ucl_nrv2e_decompress_8(input + SKIP_BYTES, sizein - SKIP_BYTES, output, &sizeout, NULL)) != 0) {
+        fprintf(stderr, "Cannot decompress; status=%d\n", status);
+    }
 
-    if (!(fwrite(output, 1, sizeout, stdout))) {
+    if (!(fwrite(output, 1, sizeout, fout))) {
         fprintf(stderr, "Cannot write to output\n");
         exit(1);
     }
+    fclose(fout);
+
+    free(input);
+    free(output);
 
     return 0;
 }
